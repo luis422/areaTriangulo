42a112cd6e98bdd0972930516c77ce8f29b6bd90
luis422 <luisgufreitas1989@gmail.com>
2021-10-28 16:30:08 -0300

    commit inicial

---
+++ area-do-triangulo.py
@@ -0,0 +1,4 @@
+base = float(input("Valores DECIMAIS, usar PONTO.\nDigite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+area = float((base*altura)/2)
+sair = input(f"A área do triângulo é: {area}")
